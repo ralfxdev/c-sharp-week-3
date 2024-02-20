6a02213eafcb24697f95a99cb13d2721afcb923b
ralfxdev <lopezalexander1019@gmail.com>
2024-02-20 00:48:32 -0600

    updated final price product

--- final-price-product/Program.cs
+++ final-price-product/Program.cs
@@ -3,25 +3,25 @@
 try
 {
     Console.WriteLine("Ingrese el precio del producto:");
-    double precio = Convert.ToDouble(Console.ReadLine());
+    double price = Convert.ToDouble(Console.ReadLine());
 
-    double precioFinal;
+    double finalPrice;
 
-    if (precio > 100)
+    if (price > 100)
     {
         // Aplicar un descuento del 10%
-        double descuento = precio * 0.10;
-        precioFinal = precio - descuento;
+        double discount = price * 0.10;
+        finalPrice = price - discount;
         Console.WriteLine("Se aplicó un descuento del 10%.");
-        Console.WriteLine($"Total del descuento Q{descuento}");
+        Console.WriteLine($"Total del descuento Q{discount}");
     }
     else
     {
-        precioFinal = precio;
+        finalPrice = price;
         Console.WriteLine("El precio no supera los Q100. No aplica descuento.");
     }
 
-    Console.WriteLine($"El precio final del producto es: Q{precioFinal}");
+    Console.WriteLine($"El precio final del producto es: Q{finalPrice}");
 }
 catch (Exception ex)
 {
