a863584ea5a3923e65ab3937fd45a195bea18d03
ralfxdev <lopezalexander1019@gmail.com>
2024-02-20 00:19:53 -0600

    add final price product

---
+++ final-price-product/Program.cs
@@ -0,0 +1,28 @@
+﻿// Solicitar el precio original del producto y aplicar un descuento del 10% si el precio es mayor a Q100. Mostrar el precio final.
+
+try
+{
+    Console.WriteLine("Ingrese el precio del producto:");
+    double precio = Convert.ToDouble(Console.ReadLine());
+
+    double precioFinal;
+
+    if (precio > 100)
+    {
+        // Aplicar un descuento del 10%
+        double descuento = precio * 0.10;
+        precioFinal = precio - descuento;
+        Console.WriteLine("Se aplicó un descuento del 10%.");
+    }
+    else
+    {
+        precioFinal = precio;
+        Console.WriteLine("El precio no supera los Q100. No aplica descuento.");
+    }
+
+    Console.WriteLine("El precio final del producto es: Q" + precioFinal);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
