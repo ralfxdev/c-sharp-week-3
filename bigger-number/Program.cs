bc1b3bc301d7c7d8481bf63b0d07759d701eeb53
ralfxdev <lopezalexander1019@gmail.com>
2024-02-19 22:54:12 -0600

    Add project files.

---
+++ bigger-number/Program.cs
@@ -0,0 +1,21 @@
+﻿double n1, n2, n3;
+
+try
+{
+    Console.WriteLine("Ingrese el primer número:");
+    n1 = Convert.ToDouble(Console.ReadLine()!);
+
+    Console.WriteLine("Ingrese el segundo número:");
+    n2 = Convert.ToDouble(Console.ReadLine()!);
+
+    Console.WriteLine("Ingrese el tercer número:");
+    n3 = Convert.ToDouble(Console.ReadLine()!);
+
+    double mayor = Math.Max(n1, Math.Max(n2, n3));
+
+    Console.WriteLine($"El mayor número ingresado es: {mayor}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
