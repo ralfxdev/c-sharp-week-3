7327243009d08ed7162635d39ed4b54ef0afabb2
ralfxdev <lopezalexander1019@gmail.com>
2024-02-20 00:13:12 -0600

    add club access

---
+++ club-access/Program.cs
@@ -0,0 +1,20 @@
+﻿// Solicitar la edad del usuario y permitir el ingreso al club solo si es mayor o igual a 18 años.
+
+try
+{
+    Console.WriteLine("Ingrese su edad:");
+    int edad = Convert.ToInt32(Console.ReadLine());
+
+    if (edad >= 18)
+    {
+        Console.WriteLine("Bienvenido al club");
+    }
+    else
+    {
+        Console.WriteLine("Lo siento, no cumples con la edad requerida para ingresar al club.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
