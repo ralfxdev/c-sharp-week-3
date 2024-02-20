99c1a9c109ade59e5f781ba4f3ce8585532bf95a
ralfxdev <lopezalexander1019@gmail.com>
2024-02-20 00:39:36 -0600

    add even or odd

---
+++ even-or-odd/Program.cs
@@ -0,0 +1,20 @@
+﻿// Solicitar un número al usuario e indicar si es par o impar.
+
+try
+{
+    Console.WriteLine("Ingrese un número:");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number % 2 == 0)
+    {
+        Console.WriteLine($"{number} es un número par.");
+    }
+    else
+    {
+        Console.WriteLine($"{number} es un número impar.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
