ff8d56af6aa05b754bd64a706afcc98f9d66c30d
ralfxdev <lopezalexander1019@gmail.com>
2024-02-20 23:00:25 -0600

    add weekdays

---
+++ weekdays/Program.cs
@@ -0,0 +1,50 @@
+﻿// Solicitar al usuario un número del 1 al 7 y mostrar el día de la semana correspondiente.
+
+try
+{
+    Console.WriteLine("Ingrese un número del 1 al 7:");
+    int nDay = Convert.ToInt32(Console.ReadLine());
+
+    string dayWeek;
+
+    switch (nDay)
+    {
+        case 1:
+            dayWeek = "Lunes";
+            break;
+
+        case 2:
+            dayWeek = "Martes";
+            break;
+
+        case 3:
+            dayWeek = "Miércoles";
+            break;
+
+        case 4:
+            dayWeek = "Jueves";
+            break;
+
+        case 5:
+            dayWeek = "Viernes";
+            break;
+
+        case 6:
+            dayWeek = "Sábado";
+            break;
+
+        case 7:
+            dayWeek = "Domingo";
+            break;
+
+        default:
+            Console.WriteLine("Número no válido.");
+            return;
+    }
+
+    Console.WriteLine($"El número {nDay} corresponde al día de la semana: {dayWeek}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ups, hubo un error {ex.Message}");
+}
